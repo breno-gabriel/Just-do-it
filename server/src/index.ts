9472d67f966a62c940d30903884daba68ea6b101
Breno <brenomelogabriel2019@gmail.com>
2025-05-09 18:00:47 -0300

    fix: fix cors problem

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,9 +6,15 @@ import express, {
 import { authRouter } from "./routes/authRoutes";
 import { userRouter } from "./routes/usersRoutes";
 import { taskRouter } from "./routes/tasksRoutes";
+import cors from "cors";
 
 const app: Express = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true 
+}));
 
 app.use("/users", userRouter);
 app.use("/auth", authRouter);

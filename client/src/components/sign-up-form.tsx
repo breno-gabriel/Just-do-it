262b39d21b57ba41f0d0ff43af4a4c2aeada41f0
Breno <brenomelogabriel2019@gmail.com>
2025-05-09 17:59:34 -0300

    feat: add login page

---
+++ client/src/components/sign-up-form.tsx
@@ -0,0 +1,111 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
+import {useNavigate} from "react-router-dom"
+
+interface loginForm {
+  email: string;
+  password: string;
+}
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const { register, handleSubmit } = useForm<loginForm>();
+  const navigate = useNavigate();
+
+  const onSubmit: SubmitHandler<loginForm> = (data) => {
+    mutate(data);
+  };
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (data: loginForm) => {
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      return response.json();
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+    onSuccess: (data) => {
+      localStorage.setItem("token", data.token);
+      navigate("/home");
+      // console.log(data);
+    },
+  });
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-xl">Bem vindo de volta</CardTitle>
+          <CardDescription>Entre com sua conta</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="grid gap-6">
+              <div className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                    {...register("email")}
+                  />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Senha</Label>
+                    <a
+                      href="#"
+                      className="ml-auto text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input
+                    id="password"
+                    type="password"
+                    required
+                    {...register("password")}
+                  />
+                </div>
+                <Button
+                  type="submit"
+                  disabled={isPending}
+                  className="w-full cursor-pointer"
+                >
+                  Entrar
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                Não tem uma conta?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Cadastre-se
+                </a>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {
+  QueryClient,
+  QueryClientProvider
+} from "@tanstack/react-query";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
+import SignUpPage from "./pages/SignUpPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
